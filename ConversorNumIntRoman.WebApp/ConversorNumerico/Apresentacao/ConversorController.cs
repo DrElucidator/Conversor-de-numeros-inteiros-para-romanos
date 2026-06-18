82616f7837e62100816a8215befd9fa766898266
DrElucidator <dr.elucidator@gmail.com>
2026-06-18 18:38:37 -0300

    Ajuste no controller para implementação de funções

--- ConversorNumIntRoman.WebApp/ConversorNumerico/Apresentacao/ConversorController.cs
+++ ConversorNumIntRoman.WebApp/ConversorNumerico/Apresentacao/ConversorController.cs
@@ -8,46 +8,91 @@ namespace ConversorNumerosInteirosRomanos.WebApp.ConversorNumerico.Apresentacao;
 public class ConversorController(ServicoConversor servicoConversor, IMapper mapeador) : Controller
 {
     [HttpGet]
-    public IActionResult Index()
+    public ActionResult Listar()
     {
-        return View();
+        List<DetalhesConversaoDto> dtos = servicoConversor.SelecionarTodos();
+        List<DetalhesConversaoViewModel> listarVms = mapeador.Map<List<DetalhesConversaoViewModel>>(dtos);
+
+        return View(listarVms);
+    }
+
+    [HttpGet]
+    public ActionResult ConverterParaRomano()
+    {
+        ConverterParaRomanoViewModel vm = new ConverterParaRomanoViewModel(0);
+        return View(vm);
     }
 
     [HttpPost]
-    public IActionResult ParaRomano(ConverterParaRomanoViewModel vm)
+    public ActionResult ConverterParaRomano(ConverterParaRomanoViewModel vm)
     {
         if (!ModelState.IsValid)
-            return View("Index", vm);
+            return View(vm);
 
-        var dto = mapeador.Map<ConverterParaRomanoDto>(vm);
+        ConverterParaRomanoDto dto = mapeador.Map<ConverterParaRomanoDto>(vm);
         Result<DetalhesConversaoDto> resultado = servicoConversor.ConverterParaRomano(dto);
 
         if (resultado.IsFailed)
         {
-            ModelState.AddModelError(string.Empty, resultado.Errors.First().Message);
-            return View("Index", vm);
+            ModelState.AddModelError(resultado.Errors.First().Metadata["Campo"]?.ToString() ?? string.Empty,
+                                     resultado.Errors.First().Message);
+            return View(vm);
         }
 
-        var detalhesVm = mapeador.Map<DetalhesConversaoViewModel>(resultado.Value);
-        return View("Index", detalhesVm);
+        DetalhesConversaoViewModel detalhesVm = mapeador.Map<DetalhesConversaoViewModel>(resultado.Value);
+        return RedirectToAction(nameof(Listar));
+    }
+
+    [HttpGet]
+    public ActionResult ConverterDeRomano()
+    {
+        ConverterDeRomanoViewModel vm = new ConverterDeRomanoViewModel(string.Empty);
+        return View(vm);
     }
 
     [HttpPost]
-    public IActionResult DeRomano(ConverterDeRomanoViewModel vm)
+    public ActionResult ConverterDeRomano(ConverterDeRomanoViewModel vm)
     {
         if (!ModelState.IsValid)
-            return View("Index", vm);
+            return View(vm);
 
-        var dto = mapeador.Map<ConverterDeRomanoDto>(vm);
+        ConverterDeRomanoDto dto = mapeador.Map<ConverterDeRomanoDto>(vm);
         Result<DetalhesConversaoDto> resultado = servicoConversor.ConverterDeRomano(dto);
 
         if (resultado.IsFailed)
         {
-            ModelState.AddModelError(string.Empty, resultado.Errors.First().Message);
-            return View("Index", vm);
+            ModelState.AddModelError(resultado.Errors.First().Metadata["Campo"]?.ToString() ?? string.Empty,
+                                     resultado.Errors.First().Message);
+            return View(vm);
         }
 
-        var detalhesVm = mapeador.Map<DetalhesConversaoViewModel>(resultado.Value);
-        return View("Index", detalhesVm);
+        DetalhesConversaoViewModel detalhesVm = mapeador.Map<DetalhesConversaoViewModel>(resultado.Value);
+        return RedirectToAction(nameof(Listar));
+    }
+
+    [HttpGet]
+    public ActionResult Excluir(Guid id)
+    {
+        Result<DetalhesConversaoDto> resultado = servicoConversor.SelecionarPorId(id);
+
+        if (resultado.IsFailed)
+        {
+            TempData["Erro"] = resultado.Errors.First().Message;
+            return RedirectToAction(nameof(Listar));
+        }
+
+        DetalhesConversaoViewModel excluirVm = mapeador.Map<DetalhesConversaoViewModel>(resultado.Value);
+        return View(excluirVm);
+    }
+
+    [HttpPost]
+    public ActionResult Excluir(DetalhesConversaoViewModel vm)
+    {
+        Result resultado = servicoConversor.Excluir(vm.Id);
+
+        if (resultado.IsFailed)
+            TempData["Erro"] = resultado.Errors.First().Message;
+
+        return RedirectToAction(nameof(Listar));
     }
 }
